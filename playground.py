ae5ba8b4c67581ebd3fa49a6dcd3bfa4b24667ad
diego <dylan@ditat.io>
2025-01-18 17:57:56 -0700

    tested get_table_cols locally and it looks fine

--- playground.py
+++ playground.py
@@ -1,3 +1,15 @@
 '''
 Test stuff
 '''
+from ditat_etl.databases import Postgres
+
+config = {
+    "database": "gfdywodjgoqjeqtcrelp",
+    "user": "postgres",
+    "password": "8yvxsZHcf2E9HsMR",
+    "host": "gfdywodjgoqjeqtcrelp.db.us-east-1.nhost.run",
+    "port": "5432"
+}
+p = Postgres(config)
+
+p.get_table_cols("account")
